52163d5ebfce06a8abfe85164a97e50e31f106c3
spvagelis <spirou.vagelis@gmail.com>
2025-01-13 15:59:41 +0200

    commit 2

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "sharedSPM",
+    platforms: [.iOS(.v15)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -19,5 +20,8 @@ let package = Package(
         .testTarget(
             name: "sharedSPMTests",
             dependencies: ["sharedSPM"]),
+        .binaryTarget(name: "shared",
+                      url: "https://github.com/spvagelis/sharedSPM/releases/download/1.0.0/shared.xcframework.zip",
+                      checksum: "3d0dd29752e41387d552edbd61c5e2d0737552210b6edc1a80d6df51d4b4f510")
     ]
 )

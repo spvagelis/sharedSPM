1bb34f4b0e481f4ac62fac562fc81410585b7e02
spvagelis <spirou.vagelis@gmail.com>
2025-01-21 14:06:30 +0200

    Created the sharedSPM dynamic library

--- Package.swift
+++ Package.swift
@@ -10,6 +10,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "sharedSPM",
+            type: .dynamic,
             targets: ["sharedSPM"]),
     ],
     targets: [

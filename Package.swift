3eb8bcc18e08e7480e8407f677124a87a03b9e17
spvagelis <spirou.vagelis@gmail.com>
2025-01-22 13:17:42 +0200

    Added Cocoalumberjack dependency

--- Package.swift
+++ Package.swift
@@ -13,21 +13,24 @@ let package = Package(
             type: .dynamic,
             targets: ["sharedSPM"]),
     ],
+    dependencies: [.package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack.git", from: "3.8.0"),
+                  ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "sharedSPM",
             dependencies: [
-                .target(name: "shared")
-            ],
-            publicHeadersPath: ""
+                .target(name: "shared"),
+                .product(name: "CocoaLumberjack", package: "CocoaLumberjack")
+            ]
         ),
         .testTarget(
             name: "sharedSPMTests",
             dependencies: ["sharedSPM"]),
         .binaryTarget(name: "shared",
                       url: "https://github.com/spvagelis/sharedSPM/releases/download/1.0.1/shared.xcframework.zip",
-                      checksum: "3d0dd29752e41387d552edbd61c5e2d0737552210b6edc1a80d6df51d4b4f510")
+                      checksum: "4cf6cc8e4e637a19726f2ec2a460033ba15c64ca748b19c6c930b4cd52c94e2e")
     ]
 )
+
